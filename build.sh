819ca9d0b0d68978c60e0a0747297038af7b0e8c
CrazyGecko <lefrantguillaume@gmail.com>
2015-04-08 10:48:20 +0200

    build.sh modified

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-#!/bin/sh
+git pull
+meteor update
+meteor build .
+sudo mv animelist.tar.gz /home/animelist/.
+cd /home/animelist/
+sudo tar -xvzf animelist.tar.gz
+sudo rm -rf bundle/
+sudo tar -xvzf animelist.tar.gz
+cd bundle/programs/server/
+sudo npm install
+cd ../../..
+sudo chown animelist -R *
+sudo service animelist restart
