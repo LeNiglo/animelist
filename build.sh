b856a613f9bf08675b0037b8af1540a3c4542e7e
CrazyGecko <lefrantguillaume@gmail.com>
2015-09-22 14:58:46 +0200

    build less verbose

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo meteor build .
 sudo mv animelist.tar.gz /opt/animelist/.
 cd /opt/animelist/
 sudo rm -rf bundle/
-sudo tar -xvzf animelist.tar.gz
+sudo tar -xzf animelist.tar.gz
 sudo rm -f animelist.tar.gz
 cd bundle/programs/server/
 sudo npm install
