2e923496ad3ac80c43411cc5b380f4cc2c223eac
Guillaume Lefrant <lefrantguillaume@gmail.com>
2016-03-23 21:59:52 +0000

    Added sorting to publication

--- server/publish.js
+++ server/publish.js
@@ -1,5 +1,5 @@
 Meteor.publish('myShows', function () {
-    return Show.find({owner: this.userId, active: true});
+    return Show.find({owner: this.userId, active: true}, {$sort: {updatedAt: -1}});
 });
 
 Meteor.publish('showNames', function () {
